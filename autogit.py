7f0b6dd068a47ba8d52a63c580e95f6b8bee7c47
ojiip <ojiip95@gmail.com>
2022-10-02 15:11:12 -0700

    adding a functions to do 2 tasks

--- autogit.py
+++ autogit.py
@@ -1,3 +1,4 @@
+from distutils.cmd import Command
 from importlib.metadata import files
 from mimetypes import init
 import subprocess 
@@ -5,18 +6,30 @@ import os
 
 
 '''
-    First, function to choose Dirctory and chec out for
-    .git file
-'''
-path = input("[+] Path: ")
 
-def change_dir():
-    newPath = os.chdir(path)
-    print("Current dir is: " + os.getcwd())
-    if os.path.isdir('.\.git') == False:
+    Local Opration --> phase [1] of the prgrame
+    task[1] --> Take user input and search in the folder for .git, 
+    if found return the new path, if don't CREATE one by COMMAND git init (TRY TO OPTEMIZE)
+
+    task[2] --> adding to the stage area and ask for user what to include in the staging area (TRY TO OPTEMIZE)
+
+'''
+path = input(str("[+] Path: "))                 
+
+def user(current):
+    
+    newpath = os.chdir(path)
+    if os.path.isdir('.\.git') == False: 
         subprocess.run(['git', 'init'])
     else:
-        print('found .git')
+        print("./git FOUND")
+    return newpath     
 
 
-change_dir()
+def take_action():      
+    user_input = input("choose file: ")
+    subprocess.run(['git', 'add', user_input])
+    subprocess.run(['git', 'status'])
+
+user(path)    
+take_action()
